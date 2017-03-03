ac43c0ab02b0409efc24453d9290d0e1a32e810e
michael mason <michael@michaeljmason.com>
2017-03-02 16:51:48 -0800

    fixed some style issues on *>1024px. added transforms for caret sub-li icons when active. refactored code appropriately. added tranforms for desktop sub-menu animation. various color and style changes

--- templates/head.php
+++ templates/head.php
@@ -4,4 +4,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,300,300italic,400italic,700,700italic,900,900italic" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 </head>
