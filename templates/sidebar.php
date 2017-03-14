05e485c7bc7b42f1be8b22ddbf015c9604470f30
michael mason <michael@michaeljmason.com>
2017-03-14 16:49:32 -0700

    creating the sidebar template and styling

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,6 +1,7 @@
   <div class="row">
     <div class="col-sm-12">
       <h5>Get In Touch</h5>
+      <div class="tile-overlay"></div>
     </div>
   </div>
   <div class="form-group row">
@@ -25,33 +26,51 @@
   </div>
   <div class="row tiles">
     <div class="col-md-6 text-center">
-      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/story_icon.png') no-repeat center center;background-size:cover;">
+      <a href="#">
+      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/ourstory.png') no-repeat center center;background-size:cover;">
         <h5>Our Story</h5>
+        <div class="tile-overlay"></div>
       </div>
+    </a>
     </div>
     <div class="col-md-6 text-center">
-      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/4199-200.png') no-repeat center center;background-size:cover;">
+      <a href="#">
+      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/legacy.png') no-repeat center center;background-size:cover;">
         <h5>Legacy of Leadership</h5>
+        <div class="tile-overlay"></div>
       </div>
+    </a>
     </div>
     <div class="col-md-6 text-center">
-      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/01/nts-admissions-homepage.jpg') no-repeat center center;background-size:cover;">
+      <a href="#">
+      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/admissions.png') no-repeat center center;background-size:cover;">
         <h5>Admissions</h5>
+        <div class="tile-overlay"></div>
       </div>
+    </a>
     </div>
     <div class="col-md-6 text-center">
-      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/Medical_staff-512.png') no-repeat center center;background-size:cover;">
+      <a href="#">
+      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/clinical.png') no-repeat center center;background-size:cover;">
         <h5>Our Clinical Team</h5>
+        <div class="tile-overlay"></div>
       </div>
+    </a>
     </div>
     <div class="col-md-6 text-center">
-      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/01/nts-addiction-homepage.jpg') no-repeat center center;background-size:cover;">
+      <a href="#">
+      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/addiction.png') no-repeat center center;background-size:cover;">
         <h5>Addiction</h5>
+        <div class="tile-overlay"></div>
       </div>
+    </a>
     </div>
     <div class="col-md-6 text-center">
-      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/01/nts-dual-homepage-1.jpg') no-repeat center center;background-size:cover;">
+      <a href="#">
+      <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/mental.png') no-repeat center center;background-size:cover;">
         <h5>Mental Health</h5>
+        <div class="tile-overlay"></div>
       </div>
+    </a>
     </div>
   </div>
