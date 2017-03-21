c6111efb29c4bc4bbc4d62a98182012436d64dd1
michael mason <michael@michaeljmason.com>
2017-03-21 15:54:48 -0700

    got to a good point with the blog roll and blog singles, going to revisit for polishing soon

---
+++ templates/blog-sidebar.php
@@ -0,0 +1,28 @@
+<?php echo do_shortcode('[contact-form-7 id="4" title="Contact form 1"]'); ?>
+<div class="row tiles">
+  <div class="col-md-12 text-center" data-aos="fade-up" data-aos-once="true">
+    <a href="#">
+    <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/people-woman-coffee-meeting-1.jpg') no-repeat center center;background-size:cover;">
+      <h3>Our Story</h3>
+      <div class="tile-overlay"></div>
+    </div>
+  </a>
+  </div>
+  <div class="col-md-12 text-center" data-aos="fade-up" data-aos-once="true">
+    <a href="#">
+    <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/pexels-photo-10604-1.jpg') no-repeat center center;background-size:cover;">
+      <h3>Legacy of Leadership</h3>
+      <div class="tile-overlay"></div>
+    </div>
+  </a>
+  </div>
+  <div class="col-md-12 text-center" data-aos="fade-up" data-aos-once="true">
+    <a href="#">
+    <div class="sidebar-tile" style="background: url('/wp-content/uploads/2017/03/pexels-photo-10604-1.jpg') no-repeat center center;background-size:cover;">
+      <h3>Legacy of Leadership</h3>
+      <div class="tile-overlay"></div>
+    </div>
+  </a>
+  </div>
+  </div>
+</div>
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,18 +1,29 @@
 <?php while (have_posts()) : the_post(); ?>
   <div class="wrap container-fluid">
-  <article <?php post_class(); ?>>
-      <h1 class="single-post-title"><?php the_title(); ?></h1>
+    <div class="row single-post-wrap">
+  <main class="col-xl-9 col-lg-8 col-md-8">
+    <article <?php post_class(); ?>>
       <div class="row">
-        <div class="col-6 offset-3">
-      <img src="<?php the_post_thumbnail_url(); ?>" class="img-fluid">
+      <div class="col-md-9 offset-md-1">
+          <img src="<?php the_post_thumbnail_url(); ?>" class="img-fluid">
+      </div>
     </div>
-  </div>
-      <?php get_template_part('templates/entry-meta'); ?>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    <!-- <?php comments_template('/templates/comments.php'); ?> -->
-  </article>
+      <h1 class="single-post-title"><?php the_title(); ?></h1>
+          <div class="blog-meta">
+            <?php get_template_part('templates/entry-meta'); ?>
+          </div>
+      <div class="entry-content">
+        <?php the_content(); ?>
+      </div>
+        <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+      <!-- <?php comments_template('/templates/comments.php'); ?> -->
+    </article>
+  </main>
+  <aside class="col-xl-3 offset-xl-0 col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-8 offset-sm-2 sidebar-main">
+    <?php do_action('get_sidebar');?>
+    <?php get_template_part('templates/blog-sidebar');?>
+  </aside>
 </div>
+</div>
+
 <?php endwhile; ?>
