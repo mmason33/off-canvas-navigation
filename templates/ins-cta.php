001edf07dda620b8d84b5737fb42aade793bc3d6
michael mason <michael@michaeljmason.com>
2017-04-25 08:40:57 -0700

    forgot to add new partial

---
+++ templates/ins-cta.php
@@ -0,0 +1,21 @@
+<div  data-aos="slide-down">
+  <div class="container-fluid wrap section-float insurance-cta">
+      <div class="container">
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <div class="row">
+                <div class="col-md-12">
+                  <h2 class="white">Most insurances accepted</h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <img class="img-fluid" src="/wp-content/uploads/2017/04/ins-background-12.png">
+                  <a href="#" class="consultation-white">Get In Touch</a>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>
+</div>
