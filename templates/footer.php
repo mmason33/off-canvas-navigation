6d2fa8f433c9a4b570ea693f2fca9ae3cef2d5f1
michael mason <michael@michaeljmason.com>
2017-05-02 16:41:48 -0700

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,57 +1,25 @@
 <?php if (!is_page_template('contact.php')): ?>
 <footer class="content-info" style="background:url('/wp-content/uploads/2017/04/footer-image-texture-idea-1.jpg') no-repeat center center;">
   <div class="container-fluid wrap">
-    <div class="row">
-      <div class="col-md-4 text-sm-center footer-column">
-        <img src="/wp-content/uploads/2017/04/footer-image-idea.png" class="img-fluid">
-        <p class="address">4343 Von Karman Ave Ste 200<br>Newport Beach, CA 92660</p>
-          <a class="contact-number" href="tel:888888888"><h4><i class="fa fa-mobile"></i> 888.888.8888</h4></a>
-          <a class="contact-number" href="https://www.google.com/maps/place/4343+Von+Karman+Ave,+Newport+Beach,+CA+92660/data=!4m2!3m1!1s0x80dcde596ca5fb65:0xed6e0bcae91255d1?sa=X&ved=0ahUKEwjnseXqh6_TAhXPq5QKHbfnD3wQ8gEIJTAB" target="_blank"><i class="fa fa-map"></i> Get Directions</a>
-      </div>
-      <div class="col-md-3 offset-md-1 footer-column">
-        <h3 class="associations-headline">Associations</h3>
-        <ul class="associations">
-          <li><h3 class="associations-title"><a class="associations-anchor" href="#">JCAHO</a><span class="associations-tooltip">Joint Commission on Accreditation of Healthcare Organizations</span></h3></li>
-
-          <li><h3 class="associations-title"><a class="associations-anchor" href="#">DHHS</a><span class="associations-tooltip">Department of Health and Human Services</span></h3></li>
-
-          <li><h3 class="associations-title"><a class="associations-anchor" href="#">NAMI</a><span class="associations-tooltip">National Alliance on Mental Illness</span></h3></li>
-
-          <li><h3 class="associations-title"><a class="associations-anchor" href="#">NAATP</a><span class="associations-tooltip">National Association of Addiction Treatment Providers</span></h3></li>
-        </ul>
-      </div>
-      <div class="col-md-3 offset-md-1 footer-column">
-        <h3 class="footer-nav-headline">More Info</h3>
-        <ul class="footer-nav">
-          <li><h3 class="footer-nav-item-title"><a class="footer-nav-anchor" href="#">Careers</a></h3></li>
-
-          <li><h3 class="footer-nav-item-title"><a class="footer-nav-anchor" href="#">Code of Ethics</a></h3></li>
-
-          <li><h3 class="footer-nav-item-title"><a class="footer-nav-anchor" href="#">Privacy Policy</a></h3></li>
-
-          <li><h3 class="footer-nav-item-title"><a class="footer-nav-anchor" href="#">Sitemap</a></h3></li>
-        </ul>
+    <div class="row justify-content-center">
+      <div class="col-md-12 text-center">
+        <h4 class="white">Our Mission Is To Assist Our Clients In Achieving One Year Of Continuous Sobriety</h4>
       </div>
     </div>
+      <div class="row justify-content-center">
+        <div class="col-md-2 text-center">
+          <a href="#"><i class="fa fa-phone-square fa-3x"></i><small>Call</small></a>
+        </div>
+        <div class="col-md-2 text-center">
+          <a href="#"><i class="fa fa-map-marker fa-3x"></i><small>Directions</small></a>
+        </div>
+        <div class="col-md-2 text-center">
+          <a href="#"><i class="fa fa-envelope fa-3x"></i><small>Contact</small></a>
+        </div>
+      </div>
   </div>
 </footer>
 <?php endif; ?>
 <?php if (is_page('home')): ?>
-<!-- Modal -->
-<div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="text-center embed-responsive embed-responsive-16by9">
-          <iframe class="embed-responsive-item" width="640" height="360" src="https://www.youtube.com/embed/I0cbrQUCHJ4" frameborder="0" allowfullscreen></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+<?php get_template_part('templates/modal'); ?>
 <?php endif; ?>
---
+++ templates/modal.php
@@ -0,0 +1,17 @@
+<!-- Modal -->
+<div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="text-center embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" width="640" height="360" src="https://www.youtube.com/embed/I0cbrQUCHJ4" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
