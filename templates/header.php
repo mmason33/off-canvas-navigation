f2ee9e7928f9416a6774e37bb0153c946bc8a302
michael mason <michael@michaeljmason.com>
2017-07-11 13:37:51 -0700

    navigation changes

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,4 @@
-<header class="header" id="nav-header">
+<!-- <header class="header" id="nav-header">
 		<div class="nav-wrap">
 			<h1 id="nav-brand"><a href="<?php echo get_site_url(); ?>">Northbound</a></h1>
       			<nav id="off-canvas-nav" class="primary-navigation">
@@ -11,9 +11,36 @@
                      'menu_class'        => 'menu dropdown',
                      'depth'             => 3
                  ));
-             ?>
-      	    </nav><!-- #off-canvas-nav -->
+             ?> -->
+      	     <!-- </nav>#off-canvas-nav -->
+    <!-- </div>.wrap -->
+<!-- </header>
+<div class="overlay"></div> -->
 
-      		</div><!-- .wrap -->
-      	</header>
-				<div class="overlay"></div>
+<header class="header container" id="nav-header">
+<div class="row justify-content-end nav-wrap">
+  <div class="col-4 text-right">
+    <a href="#">888.888.8888</a>
+  </div>
+</div>
+    <div class="nav-wrap row">
+    <div class="col-2">
+      <h1 id="nav-brand"><a href="<?php echo get_site_url(); ?>">Northbound</a></h1>
+    </div>
+    <div class="col-10">
+            <nav id="off-canvas-nav" class="primary-navigation">
+                      <a id="nav-icon" href="#" class=""><span></span></a>
+            <?php
+                 wp_nav_menu( array(
+                     'menu'              => 'Main Menu',
+                     'theme_location'    => 'primary',
+                     'menu_id'           => 'primary-menu',
+                     'menu_class'        => 'menu dropdown',
+                     'depth'             => 3
+                 ));
+             ?>
+             </nav><!-- #off-canvas-nav -->
+    </div>
+    </div><!-- .wrap -->
+</header>
+<div class="overlay"></div>
